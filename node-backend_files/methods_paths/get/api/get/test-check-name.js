1aa237f4b5f26d2ff7428769d2d27f14af3ebda6
Alejandro Aranda <alejandro.aranda@crononauta.com>
2024-02-25 17:49:07 +0100

    1.0.0

--- node-backend_files/methods_paths/get/api/get/test-check-name.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    path: '/api/get/test-check-name',
-    method: function (req, res) {
-        console.log('comprueba el nombre del check si está en uso');
-        res.send('comprueba el nombre del check si está en uso');
-    }
-};
--- node-backend_files/methods_paths/post/api/add/checks.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    path: '/api/add/checks',
-    method: function (req, res) {
-        console.log('req.body', req.body)
-        console.log('Añadiendo checks LOGIC');
-        res.send('Añadiendo checks');
-    }
-};
